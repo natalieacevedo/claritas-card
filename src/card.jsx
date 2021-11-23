a5611d4c607f973f24ec4ed1af1ca52b795dffdb
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-11-23 09:54:30 +0000

    finished Claras card

---
+++ src/card.jsx
@@ -0,0 +1,26 @@
+import './style.scss';
+import bluey from './blueyandbingo.png'
+
+function Card() {
+    
+
+
+
+
+
+
+
+
+    return (
+<div className='cardBody'>
+<img src={bluey} alt='bluey and bingo'></img>    
+<h2>Clarita is turning 3 and wants to celebrate it with you!</h2>
+            <p>Please join us on december 4th at 11 am at parque recreativo do alto da Serafina, in Monsanto </p>
+            
+<h3>No faltes :)</h3>
+</div>
+)
+
+};
+
+export default Card;
